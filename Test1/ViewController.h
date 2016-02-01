8857ae9ceac344c516cb70075d57438c86278bf8
VirginStudio <vi14@voila.fr>
2016-02-01 12:08:03 +0100

    Initial Commit

---
+++ Test1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test1
+//
+//  Created by Virginie Climent on 01/02/2016.
+//  Copyright © 2016 IT-CE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test1
+//
+//  Created by Virginie Climent on 01/02/2016.
+//  Copyright © 2016 IT-CE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
